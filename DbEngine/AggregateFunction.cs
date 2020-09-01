00e8b0596a9f9bf191b52b0026aff872467f5063
Nandita Vora <Nandita.Vora@in.niit.com>
2020-09-02 00:01:24 +0530

    initial commit

---
+++ DbEngine/AggregateFunction.cs
@@ -0,0 +1,15 @@
+﻿namespace DbEngine
+{
+    /* This class is used for storing name of field, aggregate function for 
+ * each aggregate function
+ * generate properties for this class,
+ * Also override toString method
+ * */
+    public class AggregateFunction
+    {
+        // Write logic for constructor
+        public AggregateFunction(string field, string function)
+        {
+        }
+    }
+}
---
+++ DbEngine/FilterCondition.cs
@@ -0,0 +1,17 @@
+﻿namespace DbEngine
+{
+    /*
+ * This class is used for storing name of field, condition and value for 
+ * each conditions
+ * generate properties for this class,
+ * Also override toString method
+ * */
+    public class FilterCondition
+    {
+        // Write logic for constructor
+        public FilterCondition(string propertyName,string propertyValue, string condition)
+        {
+        }
+
+    }
+}
